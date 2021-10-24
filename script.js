46fe7929f0a66212b5f9a396e1ed7700d0e6889f
Barbara Oleszek <barbaraoleszek@gmail.com>
2021-10-24 23:34:26 +0200

    game is done!

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
-const winningNumber = Math.trunc(Math.random() * 20) + 1;
+let winningNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 let check = document.getElementById(`submit`);
 let userValue;
 let userGuess = document.getElementById("user-guess");
 let tips = document.getElementById("tips");
 let tryAgain = document.getElementById("try-again");
-let score = document.getElementById("score");
-
+let seeScore = document.querySelector(".score");
+let seeHighScore = document.querySelector(".highscore");
+let userNumber = document.getElementById("user-num");
 console.log(winningNumber);
 
 function handleSubmit(event) {
   event.preventDefault();
-  let userNumber = document.getElementById("user-num");
-  if (userNumber.value > 0) {
-    userValue = userNumber.value;
-  } else {
-    alert(`Choose your number!`);
-  }
 
-  checkUserNumber(userValue);
+  if (userNumber.value > 0 && userNumber.value < 20) {
+    userValue = userNumber.value;
+    checkUserNumber(userValue);
+    userNumber.value = null;
+  } else {
+    alert(`Choose your number between 1 and 20!`);
+    userNumber.value = null;
+  }
 }
 check.addEventListener("click", handleSubmit);
 
+function handleWrongNum() {
+  if (score > 1) {
+    score--;
+    seeScore.innerHTML = `${score}`;
+  } else if (score === 1) {
+    tips.innerHTML = `You lost`;
+    seeScore.innerHTML = `0`;
+    userGuess.innerHTML = winningNumber;
+    check.disabled = true;
+  }
+}
+
+function handleWronnumtooltip(value) {
+  userGuess.innerHTML = `${userValue}`;
+  tips.innerHTML = `Your number is too ${value}`;
+}
+
 function checkUserNumber(userValue) {
   if (userValue > winningNumber) {
-    userGuess.innerHTML = `${userValue}`;
-    tips.innerHTML = `Your number is too BIG!`;
+    handleWronnumtooltip("BIG");
+    handleWrongNum();
   } else if (userValue < winningNumber) {
-    userGuess.innerHTML = `${userValue}`;
-    tips.innerHTML = `Your number is too SMALL!`;
+    handleWronnumtooltip("SMALL");
+    handleWrongNum();
   } else {
     userGuess.innerHTML = `${userValue}`;
     tips.innerHTML = `GREAT GUESS! You won!`;
+    check.disabled = true;
+    if (score > highscore) {
+      highscore = score;
+      seeHighScore.innerHTML = `${highscore}`;
+    }
   }
 }
 
 // wanna try again?
 function handleRequest(event) {
-  location.reload();
+  event.preventDefault();
+  winningNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  tips.innerHTML = `Start guessing!`;
+  seeScore.innerHTML = `${score}`;
+  userValue;
+  userGuess.innerHTML = `?`;
+  check.disabled = false;
 }
 
 tryAgain.addEventListener("click", handleRequest);
